26d018e7cff0841a796732f7ed6634ed8d3e8e3e
Jay Desai <jadesai@microsoft.com>
2018-03-08 14:23:34 -0800

    Bug fixes

--- iotedge/core/functions/updateTimeSeries/run.ps1
+++ iotedge/core/functions/updateTimeSeries/run.ps1
@@ -12,18 +12,28 @@ $tsi = $input.tsiname
 $location = $input.location
 
 $postdata = @{
-	"location"= "$location",
-	"kind"= "Microsoft.IoTHub",
+	"location"= "$location"
+	"kind"= "Microsoft.IoTHub"
 	"properties"= @{
-		"iotHubName"= "$iothub",
-		"keyName"= "iothubowner",
-		"sharedAccessKey"= "$devkey",
-		"consumerGroupName"= "`$Default",
-		"eventSourceResourceId"= "/subscriptions/$subs/resourceGroups/$resgrp/providers/Microsoft.Devices/IotHubs/$iothub",
+		"iotHubName"= "$iothub"
+		"keyName"= "iothubowner"
+		"sharedAccessKey"= "$devkey"
+		"consumerGroupName"= "`$Default"
+		"eventSourceResourceId"= "/subscriptions/$subs/resourceGroups/$resgrp/providers/Microsoft.Devices/IotHubs/$iothub"
 		"localTimestamp"= $null
 	}
 }
 
-$url = "$arm/subscriptions/$subs/resourceGroups/$resgrp/providers/Microsoft.TimeSeriesInsights/environments/$tsi/eventsources/$resgrp?api-version=2017-11-15"
+$uri = "$arm/subscriptions/$subs/resourceGroups/$resgrp/providers/Microsoft.TimeSeriesInsights/environments/$tsi/eventsources/$resgrp" + "?api-version=2017-11-15"
 $body = $postdata | Convertto-Json
-$resp = Invoke-RestMethod "$uri" -Method POST -Body $body -ContentType "application/json" -Headers @{"Authorization"="Bearer $authtoken"}
+
+$uri | Out-File "D:\\home\\ciqs\\log.txt" -Append
+$body | Out-File "D:\\home\\ciqs\\log.txt" -Append
+
+try{
+$resp = Invoke-RestMethod "$uri" -Method PUT -Body $body -ContentType "application/json" -Headers @{"Authorization"="Bearer $authtoken"}
+} catch {
+    $_.Exception.ErrorDetails | Out-File "D:\\home\\ciqs\\log.txt" -Append
+    $_.Exception.Response | Out-File "D:\\home\\ciqs\\log.txt" -Append
+    throw
+}
