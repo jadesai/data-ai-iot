80821d6cacee8289f6f0a0c53627f3ab99191c44
Jay Desai <jadesai@microsoft.com>
2018-03-19 17:09:55 -0700

    Simulation branch changes

--- iotedge/core/functions/updateTimeSeries/run.ps1
+++ iotedge/core/functions/updateTimeSeries/run.ps1
@@ -27,13 +27,4 @@ $postdata = @{
 $uri = "$arm/subscriptions/$subs/resourceGroups/$resgrp/providers/Microsoft.TimeSeriesInsights/environments/$tsi/eventsources/$resgrp" + "?api-version=2017-11-15"
 $body = $postdata | Convertto-Json
 
-$uri | Out-File "D:\\home\\ciqs\\log.txt" -Append
-$body | Out-File "D:\\home\\ciqs\\log.txt" -Append
-
-try{
 $resp = Invoke-RestMethod "$uri" -Method PUT -Body $body -ContentType "application/json" -Headers @{"Authorization"="Bearer $authtoken"}
-} catch {
-    $_.Exception.ErrorDetails | Out-File "D:\\home\\ciqs\\log.txt" -Append
-    $_.Exception.Response | Out-File "D:\\home\\ciqs\\log.txt" -Append
-    throw
-}
